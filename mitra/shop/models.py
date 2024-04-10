a76c233bb46598ebbca2e0c3a3bd4a97b3ba2cad
nelson <htop000@yandex.ru>
2024-04-11 01:06:00 +0300

    Added apps shop, users

---
+++ mitra/shop/models.py
@@ -0,0 +1,23 @@
+# from django.db import models
+#
+# # Create your models here.
+# class Biz(models.Model):
+#     metro: # text
+#     ploshchad: # kv.m
+#     vozrast: # let
+#     vyruchka: # tys.r.v
+#     chistaya pribyl: # tys.r./g.
+#     arenda: # tys.r. / mes.
+#     remont: # 1 - 5⭐
+#     Okupaemost: # mes.
+#     do_metro_min: # min.
+#     do_metro_m:  # m.
+#     kol_vo sotr.: # chel.
+#     stoimost ostatkov: # tys. r.
+#     stoimost oborudovaniya: # tys. r.
+#     kol_vo chekov: # sht.
+#     sr.chek: # r.
+#     opisanie: # tekstovoe pole
+#     cena: # tys.r.
+#     nazvanie: # stroka
+#     rejting_na_ya_karte: # 1 - 5⭐
