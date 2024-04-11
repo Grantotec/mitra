9e1865406b81a3ce1ccddc1fa0e2ecbd0f20eb09
nelson <htop000@yandex.ru>
2024-04-11 21:36:40 +0300

    deleted .idea

--- mitra/shop/models.py
+++ mitra/shop/models.py
@@ -1,4 +1,13 @@
-# from django.db import models
+from django.db import models
+
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+ class User(AbstractUser):
+     pass
+
 #
 # # Create your models here.
 # class Biz(models.Model):
